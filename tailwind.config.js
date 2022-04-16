d4d1782db694052791395ced8c843f3c35fd5286
Dipak Parmar <hi@dipak.tech>
2022-04-16 10:14:03 +0000

    add external Google Fonts to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Karla", "Inter", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {},
   },
   variants: {
