d692d317c673cdad01cdedfd1832b684ef62b506
Dipak Parmar <hi@dipak.tech>
2022-05-18 10:24:44 -0700

    🚩 add links from linktree

---
+++ hasura/migrations/default/1652893490692_insert_links_from_linktree/down.sql
@@ -0,0 +1,6 @@
+DELETE FROM "public"."links" WHERE "id" = '4b70fea1-883b-416b-9e9f-0774360f85c1';
+DELETE FROM "public"."links" WHERE "id" = 'df526da8-d6ca-11ec-9d64-0242ac120002';
+DELETE FROM "public"."links" WHERE "id" = 'df526f9c-d6ca-11ec-9d64-0242ac120002';
+DELETE FROM "public"."links" WHERE "id" = 'df5270c8-d6ca-11ec-9d64-0242ac120002', 
+DELETE FROM "public"."links" WHERE "id" = 'df5271d6-d6ca-11ec-9d64-0242ac120002';
+DELETE FROM "public"."links" WHERE "id" = 'df5271d9-d6ca-11ec-9d64-0242ac120002'; 
