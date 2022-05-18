096c35110b050da94edb346f64938f7e3a043b53
Dipak Parmar <hi@dipak.tech>
2022-05-18 09:41:06 -0700

    🗃️ create public.link_type enum table with http as first type

---
+++ hasura/migrations/default/1652891685346_create_table_public_link_type/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."link_type" ("name" text NOT NULL, PRIMARY KEY ("name") , UNIQUE ("name"));COMMENT ON TABLE "public"."link_type" IS E'link_type enum table';
---
+++ hasura/migrations/default/1652891861661_insert_into_public_link_type/up.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."link_type"("name") VALUES (E'http');
