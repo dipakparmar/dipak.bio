44421aa7a01b1cadb7d10bc65f8987ce4e8e1706
Dipak Parmar <hi@dipak.tech>
2022-05-18 19:07:09 -0700

    ♻️ load ga only on production

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,12 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Script
+			{process.env.NEXT_PUBLIC_GA_TAG_ID && process.env.NODE_ENV === 'production' ? <><Script
 				strategy="lazyOnload"
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG_ID}`}
 			/>
-
-			<Script id="google-analytics" strategy="lazyOnload">
-				{`
+				<Script id="google-analytics" strategy="lazyOnload">
+					{`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -20,7 +19,7 @@ function MyApp({ Component, pageProps }) {
               page_path: window.location.pathname,
             });
                 `}
-			</Script>
+				</Script> </> : null}
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
