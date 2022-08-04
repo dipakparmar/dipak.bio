3ccb44ceb46a53e6db06687173b9fa5f263bdf3e
Dipak Parmar <hi@dipak.tech>
2022-08-04 12:31:21 -0700

    🐛 fix(api): handle undefined secret and better error logging

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,15 +1,20 @@
 export default async function handleWebhook(req, res) {
 	console.log(`[Next.js] Webhook call received from ${req.headers.host}`);
+
+	console.log(`[Next.js] Validating webhook...`);
 	// Check for secret to confirm this is a valid request
-  if (req.query.secret == process.env.NEXT_PUBLIC_WEBHOOK_SECRET) {
+  if (req.query.secret !== undefined && req.query.secret === process.env.NEXT_PUBLIC_WEBHOOK_SECRET) {
+		console.log(`[Next.js] Webhook secret validated`);
     try {
-			console.log(`[Next.js] Revalidating /`);
+			console.log(`[Next.js] Revalidating url /`);
 			await res.unstable_revalidate('/')
-			console.log(`[Next.js] Revalidated /`);
+			console.log(`[Next.js] Revalidated url /`);
 			return res.json({ revalidated: true })
 		} catch (err) {
 			// If there was an error, Next.js will continue
 			// to show the last successfully generated page
+			console.log(`[Next.js] Error revalidating url /`);
+			console.log(err);
 			return res.status(500).send('Error revalidating')
 		}
   }
